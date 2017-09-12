2a0cbb37f30fce241e08087574a031ffc81a6870
rektide de la faye <rektide@voodoowarez.com>
2017-09-12 06:08:23 -0400

    Properly resolve hostnames.

--- pingmulti.js
+++ pingmulti.js
@@ -2,66 +2,67 @@
 "use strict"
 var
   byline= require( "byline"),
+  dns= require( "dns"),
   most= require( "most"),
   pmap= require( "most-pmap"),
   mri= require( "mri"),
-  ping= require( "net-ping")
+  ping= require( "net-ping"),
+  promisify= require( "es6-promisify"),
+  dnsResolve= promisify(dns.resolve4, dns)
 
 function args( argv){
 	argv= argv|| process.argv.slice( 2)
 	return mri(argv, {
 		alias: {
-			timeout: 't',
-			count: 'c',
-			parallel: 'p'
-		},
-		default: {
-			parallel: 4
+			timeout: "t",
+			parallel: "p"
 		}
 	})
 }
 
-function times( x, n){
-	var result= new Array( n)
-	for( var i= 0; i< n; ++i){
-		console.log("el", i, n)
-		result[ i]= x
+function pickRandom( arr){
+	var i= Math.floor( Math.random()* arr.length)
+	return arr[i]
+}
+
+var _ip= /\(d{1,3}\.\d{1,3}\.\(d{1,3}\.\d{1,3}/
+async function findIp( address){
+	if( _ip.test( address)){
+		return address
 	}
-	return result
+	return await dnsResolve( address).then( pickRandom)
 }
 
 function run(lines, args){
+	async function runPing( address){
+		var ip= await findIp( address)
+		return new Promise(( resolve, reject)=> {
+			session.pingHost( ip,( err, target, sent, recvd)=> {
+				if( err){
+					reject( err)
+					return
+				}
+				var ms= recvd.getTime()- sent.getTime()
+				resolve({ ms, address})
+			})
+		})
+	}
+
 	if( !lines){
 		process.stdin.setEncoding( "utf8")
 		lines= byline.createStream( process.stdin)
 	}
 	args= args|| module.exports.args()
 	var
-	  parallel= parseInt(args.parallel)|| 4,
+	  parallel= parseInt(args.parallel)|| 2,
 	  session= ping.createSession( args),
 	  lineStream= most.fromEvent( 'data', lines).multicast(),
-	  lineStreams= most.from( times( lineStream, args.count|| 1)),
-	  scanout= most.mergeConcurrently(1, lineStreams),
-	  output= pmap(function( address){
-		console.log({address})
-		return new Promise(( resolve, reject)=> {
-			console.log("trying", address)
-			session.pingHost( address, (err, target, sent, recvd)=> {
-				if( err){
-					reject(err)
-					return
-				}
-				var ms= recvd.getTime()- sent.getTime()
-				console.log("resolve", ms, address)
-				resolve({ms, address})
-			})
-		})
-	  }, scanout, parallel)
+	  output= pmap(runPing, lineStream, parallel)
 	return output
 }
 
 function main(){
-	run().forEach(x=> console.log(x.ms, x.address, x))
+	run().forEach(x=> console.log(x.ms.toString(), x.address))
 }
 
 module.exports= run
